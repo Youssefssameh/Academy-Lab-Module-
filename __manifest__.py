28d4b5bbba5cadb08d03784ca67b6bb3310422b5
youssefssameh <x2ullahx@gmail.com>
2025-12-31 14:58:47 +0200

    adjust orderng and adding new XML iles

--- __manifest__.py
+++ __manifest__.py
@@ -17,17 +17,28 @@
         'security/academy_security.xml',
         'security/ir.model.access.csv',
         'security/academy_record_rules.xml',
-        
+
         'wizard/academy_product_wizard_view.xml',
-        
+
+        # Reports (must be loaded before views that reference them by xmlid)
+        'reports/report_actions.xml',
+        'reports/report_templates.xml',
+
+        # Views
         'views/academy_actions.xml',
         'views/academy_course_views.xml',
         'views/academy_category_views.xml',
         'views/academy_enrollment_views.xml',
-        'views/academy_menus.xml',
         'views/res_partner_views.xml',
         'views/product_template_view.xml',
+
+        # Menus last
+        'views/academy_menus.xml',
+
     ],
+    'assets': {
+        'web.report_assets_common': ['academy_lab/static/src/css/font.css']
+    },
     'installable': True,
     'application': True,
     'license': 'LGPL-3',
